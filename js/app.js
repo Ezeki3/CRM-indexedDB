b6f013dfb05782f47277596b8f9af727b100a587
Ezequiel Lino <sonisombra@gmail.com>
2023-05-31 10:57:28 -0600

    changes

--- js/app.js
+++ js/app.js
@@ -7,4 +7,15 @@
   });
 
   // crea la base de datos
+  function crearDB() {
+    const crearDB = window.indexedDB.open('crm', 1);
+
+    crearDB.onerror = function () {
+      console.log('Hubo un error');
+    };
+
+    crearDB.onsuccess = function () {
+      DB = crearDB.result;
+    }
+  }
 })
