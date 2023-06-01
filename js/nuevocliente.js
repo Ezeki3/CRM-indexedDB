7f845a67b74c2c52f4f8c8bd77b6e281d58af1ea
Ezequiel Lino <sonisombra@gmail.com>
2023-06-01 10:47:42 -0600

    changes

---
+++ js/nuevocliente.js
@@ -0,0 +1,13 @@
+// IIFE
+(function(){
+  let DB;
+  const formulario = document.querySelector('#formulario');
+
+  document.addEventListener('DOMContentLoaded', () =>{
+    conectarDB();
+  });
+
+
+
+
+})();
