ebfc681806277034afd3e5df13be58a6fece92ce
Ezequiel Lino <sonisombra@gmail.com>
2023-06-01 11:08:18 -0600

    changes

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -38,5 +38,21 @@
 
   }
 
+  function imprimirAlerta(mensaje, tipo){
+    // crear la alerta
+    const divMensaje = document.createElement('div');
+    divMensaje.classList.add('px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center', 'border');
+
+    if (tipo === 'error') {
+      divMensaje.classList.add('bg-red-100', 'border-red-400', 'text-red-700');
+    } else {
+      divMensaje.classList.add('bg-green-100', 'border-green-400','text-green-700');
+    }
+
+    divMensaje.textContent = mensaje;
+
+    formulario.appendChild(divMensaje);
+  }
+
 
 })();
