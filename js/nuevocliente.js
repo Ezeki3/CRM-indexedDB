1818a299aeb947da59754a20a524bfb6f36ce397
Ezequiel Lino <sonisombra@gmail.com>
2023-06-01 10:50:43 -0600

    changes

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -5,9 +5,21 @@
 
   document.addEventListener('DOMContentLoaded', () =>{
     conectarDB();
+
+    formulario.addEventListener('submit', validarCliente);
   });
 
+  function conectarDB(){
+    const abrirConexion = window.indexedDB.open('crm', 1);
 
+    abrirConexion.onerror = function(){
+      console.log('Error al abrir la conexión a la base de datos');
+    };
+
+    abrirConexion.onsuccess = function(){
+      DB = abrirConexion.result;
+    }
+  }
 
 
 })();
