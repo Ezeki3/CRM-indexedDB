0a4314eb2da8f5ae434f84fc22859ca3e2b913bb
Ezequiel Lino <sonisombra@gmail.com>
2023-06-01 10:58:26 -0600

    changes

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -21,5 +21,22 @@
     }
   }
 
+  function validarCliente(e){
+    e.preventDefault();
+
+    // leer todos los inputs
+    const nombre = document.querySelector('#nombre').value;
+    const email = document.querySelector('#email').value;
+    const telefono = document.querySelector('#telefono').value;
+    const empresa = document.querySelector('#empresa').value;
+
+    if (nombre === '' || email === '' || telefono === '' || empresa === '' ) {
+      imprimirAlerta('Todos los campos son obligatorios', 'error');
+
+      return;
+    }
+
+  }
+
 
 })();
