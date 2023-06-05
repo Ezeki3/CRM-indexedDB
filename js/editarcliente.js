67a4cf5ec598f44344e593aa22f131f51b466cc2
Ezequiel Lino <sonisombra@gmail.com>
2023-06-05 11:16:56 -0600

    changes

--- js/editarcliente.js
+++ js/editarcliente.js
@@ -6,8 +6,14 @@
   const telefonoInput = document.querySelector('#telefono');
   const empresaInput = document.querySelector('#empresa');
 
+  const formulario = document.querySelector('#formulario');
+
+
   document.addEventListener('DOMContentLoaded', () => {
     conectarDB();
+
+    // Actuliza el registro
+    formulario.addEventListener('submit', actulizarCliente);
 
     // Verificar el ID de la URL
     const parametrosURL = new URLSearchParams(window.location.search);
@@ -16,10 +22,20 @@
       // para que le demos tiempo a buscar la DB o usar programacion asincrona
       setTimeout(() => {
         obtenerCliente(idCliente);
-      }, 1000);
+      }, 100);
     }
 
   });
+
+  function actulizarCliente(e) {
+    e.preventDefault();
+
+    if (nombreInput.value === '' || emailInput.value === '' || telefonoInput.value === '' || empresaInput.value === '') {
+      console.log('error');
+      return;
+    }
+
+  }
 
   function obtenerCliente(id) {
     const transaction = DB.transaction(['crm'], 'readwrite');
@@ -42,9 +58,12 @@
   }
 
   function llenarFormulario(datosCliente) {
-    const { nombre } = datosCliente;
+    const { nombre, email, telefono, empresa } = datosCliente;
 
     nombreInput.value = nombre;
+    emailInput.value = email;
+    telefonoInput.value = telefono;
+    empresaInput.value = empresa;
   }
 
   function conectarDB() {
