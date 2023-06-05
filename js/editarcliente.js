bcb7f27bac7d5198895fd819276d9dacde8e23bd
Ezequiel Lino <sonisombra@gmail.com>
2023-06-05 10:15:37 -0600

    changes

--- js/editarcliente.js
+++ js/editarcliente.js
@@ -1,12 +1,34 @@
 (function () {
+  let DB;
 
   document.addEventListener('DOMContentLoaded', () => {
+    conectarDB();
 
     // Verificar el ID de la URL
     const parametrosURL = new URLSearchParams(window.location.search);
     const idCliente = parametrosURL.get('id');
-    console.log(idCliente);
-  })
+    if (idCliente) {
+      obtenerCliente(idCliente);
+    }
+
+  });
+
+  function obtenerCliente(id) {
+    console.log(id);
+  }
+
+  function conectarDB() {
+    const abrirConexion = window.indexedDB.open('crm', 1);
+
+    abrirConexion.onerror = function () {
+      console.log('Error al abrir la conexión a la base de datos');
+    };
+
+    abrirConexion.onsuccess = function () {
+      DB = abrirConexion.result;
+    }
+
+  }
 
 
 })();
